e6cf11734f1edde0420cf86645aca7362609a3f8
Kostas <langas.kostya@mail.ru>
2022-08-23 12:11:27 +0500

    вывод всех натуральных чисел в промежутке от 1 до N с помощью рекурсии

---
+++ Задача63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// с помощью рекурсии
+
+string f(int n)
+{
+    if (n == 1)
+        return "1 ";
+
+    return f(n-1) + ", " + Convert.ToString(n);
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
+
