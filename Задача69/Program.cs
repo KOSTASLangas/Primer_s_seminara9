1feae6d666f6f88408811a8c33bb3fafc0207cce
Kostas <langas.kostya@mail.ru>
2022-08-24 09:26:47 +0500

    возведение числа в степень другого числа с помощью рекурсии

---
+++ Задача69/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа А и В, и возводит число А в целую степень В 
+// с помощью рекурсии
+
+int f(int n, int m)
+{
+    if (m == 0)
+        return 1;
+
+    return f(n, m-1) * n;
+}
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n, m));
