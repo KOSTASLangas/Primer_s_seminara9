15301999e070059eb2342c9480b0cd976a5eec31
Kostas <langas.kostya@mail.ru>
2022-08-24 10:16:18 +0500

    вывод последовательности введенной с клавы, кол-во элементов равно n

---
+++ Задача156/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано натуральное число N и последовательность из N элементов. 
+// Требуется вывести эту последовательность в обратном порядке без массивов и циклов.
+
+string f(int n)
+{
+    if (n == 0)
+        return "";
+
+    int k = Convert.ToInt32(Console.ReadLine());
+    return f(n-1)  + " " + Convert.ToString(k);
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
